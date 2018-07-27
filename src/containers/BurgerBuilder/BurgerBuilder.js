5182aa178d7074527bc45a592bdb44290d908310
Siarhei Kremis <Siarhei_Kremis@epam.com>
2018-07-27 17:49:52 +0300

    L96: implementation of burger conatainer

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Ax from '../../hoc/Ax';
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Ax>
+        <div>Burger</div>
+        <div>Build controls</div>
+      </Ax>
+    );
+  }
+}
+
+export default BurgerBuilder;
