d254529cdcd842656ec1e4a1c5a0430a9ed8a027
Siarhei <dreamstar.pwr@gmail.com>
2018-07-29 16:41:18 +0300

    L105: removing ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,65 @@
 import React, { Component } from 'react';
+
 import Ax from '../../hoc/Ax';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+
+const INGEDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 1.7
+};
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    },
+    totalPrice: 4
+  }
+
+  addIngredient = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {...this.state.ingredients};
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGEDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+  }
+
+  removeIngredient = (type) => {
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) {
+      return;
+    }
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = {...this.state.ingredients};
+    updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGEDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+  }
+
   render() {
+    const disabledInfo = {...this.state.ingredients};
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
+
     return (
       <Ax>
-        <div>Burger</div>
-        <div>Build controls</div>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls 
+          ingredientAdded={this.addIngredient}
+          ingredientRemoved={this.removeIngredient}
+          disabled={disabledInfo} />
       </Ax>
     );
   }
