693b41e6fa9ba3b6c7b513034ebd3279524dd035
Siarhei Kremis <Siarhei_Kremis@epam.com>
2018-07-30 18:02:53 +0300

    L129: HTTP requests

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Ax from '../../../hoc/Ax';
+import Ax from '../../../hoc/Ax/Ax';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
+class OrderSummary extends Component {
+  // This could be a functional component, doesn't have to be a class
+  componentWillUpdate() {
+    console.log('orderSummary will update');
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map(igKey => {
       return (
-      <li key={igKey}>
-        <span style={{textTransform: 'capitalize'}}>{igKey}: {props.ingredients[igKey]}</span>
-      </li> );
+        <li key={igKey}>
+          <span style={{textTransform: 'capitalize'}}>{igKey}: {this.props.ingredients[igKey]}</span>
+        </li> );
     });
-  
 
-  return (
-    <Ax>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to checkout?</p>
-      <Button btnType='Danger' clicked={props.purchaseCanceled}>Cancel</Button>
-      <Button btnType='Success' clicked={props.purchaseContinued}>Continue</Button>
-    </Ax>
-  )
-};
+    return (
+      <Ax>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to checkout?</p>
+        <Button btnType='Danger' clicked={this.props.purchaseCanceled}>Cancel</Button>
+        <Button btnType='Success' clicked={this.props.purchaseContinued}>Continue</Button>
+      </Ax>
+    )
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.css';
 
 const logo = (props) => (
-  <div className={classes.Logo}>
+  <div className={classes.Logo} style={{height: props.height}}>
     <img alt='Burger logo' src={burgerLogo} />
   </div>
 );
